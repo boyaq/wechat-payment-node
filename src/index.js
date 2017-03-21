51390c4c2664548d2d2bdcc763d3df181fbc0c4d
boyaq <boyaq@otkur.biz>
2017-03-21 19:11:39 +0800

    fixed entry of the project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/*!
+ * wechat payment node for app and jsapi payment
+ * Copyright(c) 2013-2017 Alim Boyaq <boyaq@otkur.biz>
+ * MIT Licensed
+ */
+
+import WechatPayment from './WechatPayment';
+import WechatPaymentForWeb from './WechatPaymentForWeb';
+
+
+
+export {
+	WechatPaymentForWeb,
+}
+
+export default WechatPayment;
